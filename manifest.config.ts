7c9440f60e01a105bf3ca6729214306ca22fe90c
Eric Le <eriiicle@outlook.com>
2026-02-12 21:33:33 +0100

    feat(manifest): add storage permission

--- manifest.config.ts
+++ manifest.config.ts
@@ -15,7 +15,7 @@ export default defineManifest({
     },
     default_popup: 'src/popup/index.html',
   },
-  permissions: ['sidePanel', 'contentSettings'],
+  permissions: ['sidePanel', 'contentSettings', 'storage'],
   content_scripts: [
     {
       js: ['src/content/main.tsx'],
