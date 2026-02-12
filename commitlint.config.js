b5f2642dd1df88cb3b0b486c418e04e8a6e58573
Eric Le <eriiicle@outlook.com>
2026-02-12 20:36:45 +0100

    chore(commitlint): update linting rules for subject and scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,10 @@
 const config = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [2, 'always', 'lower-case'],
+    'subject-case': [0],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
   },
 }
 
