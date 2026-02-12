c177470c5d35d16b348e58f189e19c78c0bf5574
Eric Le <eriiicle@outlook.com>
2026-02-12 21:34:04 +0100

    feat(manifest): register background service worker

--- manifest.config.ts
+++ manifest.config.ts
@@ -16,6 +16,10 @@ export default defineManifest({
     default_popup: 'src/popup/index.html',
   },
   permissions: ['sidePanel', 'contentSettings', 'storage'],
+  background: {
+    service_worker: 'src/background/index.ts',
+    type: 'module',
+  },
   content_scripts: [
     {
       js: ['src/content/main.tsx'],
