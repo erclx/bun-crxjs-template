39b55fba087fbd55bea83bad93a47e424e957322
Eric Le <eriiicle@outlook.com>
2026-02-26 08:34:26 +0100

    style(build): normalize path import

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import path from 'node:path'
-
 import { crx } from '@crxjs/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 import zip from 'vite-plugin-zip-pack'
 
